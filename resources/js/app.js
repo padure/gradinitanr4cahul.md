02330994eadfa93483afab5ca0acdbf9352a4507
padure <paduregheorghe7@gmail.com>
2023-05-01 15:23:33 +0300

    New template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,84 @@
-import './bootstrap';
-import '../css/app.css';
+import 'animate.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+import $ from './jquery.js';
+// import './bootstrap';
+import './easing';
+import 'waypoints/src/waypoint';
+import 'owl.carousel';
+// import '../css/app.css';
+// Initiate the wowjs
+new WOW().init();
+
+
+// Spinner
+var spinner = function () {
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
+};
+spinner();
+
+
+// Sticky Navbar
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    } else {
+        $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+    }
+});
+
+
+// Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+// Header carousel
+$(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>'
+    ]
+});
+
+
+// Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 24,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        992: {
+            items: 2
+        }
+    }
+});
