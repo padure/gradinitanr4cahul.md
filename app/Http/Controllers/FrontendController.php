6744248f230d6ebcbeb84032cce0c6d9a37f4608
padure <paduregheorghe7@gmail.com>
2023-05-02 07:16:05 +0300

    rute

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,4 +9,7 @@ class FrontendController extends Controller
     public function index(){
         return view('frontend.home.index');
     }
+    public function about(){
+        return view('frontend.about.about');
+    }
 }
--- routes/frontend/web.php
+++ routes/frontend/web.php
@@ -3,3 +3,10 @@
     use App\Http\Controllers\FrontendController;
     //Frontend
     Route::get('/', [FrontendController::class, 'index'])->name('home.index');
+    Route::get('/despre-noi', [FrontendController::class, 'about'])->name('about.index');
+    Route::get('/contacte', [FrontendController::class, 'contacts'])->name('contacts.index');
+    Route::get('/evenimente', [FrontendController::class, 'event'])->name('event.index');
+    Route::get('/galerie', [FrontendController::class, 'gallery'])->name('gallery.index');
+    Route::get('/meniu', [FrontendController::class, 'menu'])->name('menu.index');
+    Route::get('/regimul-zilei', [FrontendController::class, 'regime'])->name('regime.index');
+    Route::get('/echipa-noastra', [FrontendController::class, 'team'])->name('team.index');
